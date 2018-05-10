58609a067ee507786a7a3b97506c8f31f239baf2
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-05-10 23:20:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,26 +1,23 @@
-#include "tree.cpp"
+#include "bstree.cpp"
 
 using namespace BSTree;
-void menu(Tree*, bool); //меню
+void menu(Tree*, bool);  //меню
 int main(int argc, char* argv[]) {
-    
-    setlocale(LC_ALL, "Rus");
-    bool empty = true; //для пустого дерева
-    
-    Tree tree; //наше дерево
-    
-    bool suc = false; //проверка успешности добавления элементов в дерево
-    if (argc > 1) {
-        for (unsigned int i = 1; i < argc; i++)
-            suc = tree.insert(atoi(argv[i]));
-        if (suc)
-            cout << "ДЕРЕВО УСПЕШНО СФОРМИРОВАНО" << endl;
-        else
-            cout << "ПОВТОРЯЮЩИЕСЯ ЭЛЕМЕНТЫ УДАЛЕНЫ" << endl;
-        empty = false;
-    }
+  setlocale(LC_ALL, "Rus");
+  bool empty = true;  //для пустого дерева
+
+  Tree tree;  //наше дерево
+
+  bool suc = false;  //проверка успешности добавления элементов в дерево
+  if (argc > 1) {
+    for (unsigned int i = 1; i < argc; i++) suc = tree.insert(atoi(argv[i]));
+    if (suc)
+      cout << "ДЕРЕВО УСПЕШНО СФОРМИРОВАНО" << endl;
     else
-        empty = true;
-    
-    menu(&tree, empty);
+      cout << "ПОВТОРЯЮЩИЕСЯ ЭЛЕМЕНТЫ УДАЛЕНЫ" << endl;
+    empty = false;
+  } else
+    empty = true;
+
+  menu(&tree, empty);
 }
