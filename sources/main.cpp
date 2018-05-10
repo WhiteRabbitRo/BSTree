0b30d514d831f7425aa9151185a4f6939dacd5ec
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-05-10 23:19:21 +0300

    Add files via upload

---
+++ sources/main.cpp
@@ -0,0 +1,26 @@
+#include "tree.cpp"
+
+using namespace BSTree;
+void menu(Tree*, bool); //меню
+int main(int argc, char* argv[]) {
+    
+    setlocale(LC_ALL, "Rus");
+    bool empty = true; //для пустого дерева
+    
+    Tree tree; //наше дерево
+    
+    bool suc = false; //проверка успешности добавления элементов в дерево
+    if (argc > 1) {
+        for (unsigned int i = 1; i < argc; i++)
+            suc = tree.insert(atoi(argv[i]));
+        if (suc)
+            cout << "ДЕРЕВО УСПЕШНО СФОРМИРОВАНО" << endl;
+        else
+            cout << "ПОВТОРЯЮЩИЕСЯ ЭЛЕМЕНТЫ УДАЛЕНЫ" << endl;
+        empty = false;
+    }
+    else
+        empty = true;
+    
+    menu(&tree, empty);
+}
