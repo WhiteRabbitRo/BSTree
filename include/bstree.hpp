e0014b329241ddd9ace39201e03f3f878c352ba8
Robert <34191226+WhiteRabbitRo@users.noreply.github.com>
2018-05-13 00:51:00 +0300

    Update bstree.hpp

--- include/bstree.hpp
+++ include/bstree.hpp
@@ -1,34 +1,36 @@
-namespace BSTree {
-struct Node {
-  int data;
-  Node* left;   //ветка влево
-  Node* right;  //ветка вправо
-  Node(int val) : data{val}, left{nullptr}, right{nullptr} {};  //конструтор
-};
+namespace BSTree
+{
+    struct Node {
+        int data;
+        Node* left; //ветка влево
+        Node* right; //ветка вправо
+        Node(int val) : data{val}, left{nullptr}, right{nullptr} {}; //конструтор
+    };
 }
-namespace BSTree {
-enum traversal_order { direct, symmetric, reverse };
-class Tree {  //класс дерево
- public:
-  Tree() : root{nullptr} {};              //конструктор
-  Tree(std::initializer_list<int> list);  // Лист инициализации
-  Tree(const Tree& tree);  //конструктор копирования
-  void print_tree();       //вывод дерева
-  bool exists(int val);    //поиск узла
-  bool insert(int val);    //добавление узла
-  bool remove(int val);    //удаление узла
-  bool save(const std::string& path);  //запись дерева в файл
-  bool load(const std::string& path);  //загрузка дерева из файла
-  void print(traversal_order ord);  //вывод узлов тремя способами
-  auto friend operator<<(std::ostream& stream, const Tree&)
-      -> std::ostream&;  //оператор вывода в поток
-  auto operator=(const Tree& tree) -> Tree&;  //оператор присваивания
-  ~Tree() {                                   //деструктор
-    clean(root);
-  };
-
- private:
-  Node* root;
-  void clean(Node*& node);  //для удаления
-};
+namespace BSTree
+{
+    enum traversal_order { direct, symmetric, reverse};
+    class Tree { //класс дерево
+    public:
+        Tree() : root{nullptr} {}; //конструктор
+        Tree (std::initializer_list<int> list); // Лист инициализации
+        Tree(const Tree& tree); //конструктор копирования
+        void print_tree(); //вывод дерева
+        bool empty_check(); //проверка дерева на пустоту
+        bool exists(int val); //поиск узла
+        bool insert(int val); //добавление узла
+        bool remove(int val); //удаление узла
+        bool save(const std::string& path); //запись дерева в файл
+        bool load(const std::string& path); //загрузка дерева из файла
+        void print(traversal_order ord); //вывод узлов тремя способами
+        auto friend operator<<(std::ostream& stream, const Tree&) -> std::ostream&; //оператор вывода в поток
+        auto operator=(const Tree& tree) -> Tree&;  //оператор присваивания
+        ~Tree() { //деструктор
+            clean(root);
+        };
+    private:
+        Node* root;
+        void clean(Node*& node); //для удаления
+    };
+    void swap(Tree&, Node*); //поменять значения узлов деревьев
 }
